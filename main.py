cd222a547ebf406648f83876f9a74d82ce480746
cristinel24 <cristianandrei987@gmail.com>
2024-04-02 21:32:14 +0300

    [tutorial] test

---
+++ main.py
@@ -0,0 +1,27 @@
+
+if __name__ == '__main__':
+    test_dict: dict = {
+        "cooking": "app",
+        "developing": True,
+        "product_manager": "iceeye7",
+        "teams": {
+            "frontend": {
+                "number_of_members": 9,
+                "scrums_masters": ["Ana Maria Ursache"]
+            },
+            "backend": {
+                "number_of_members": 15,
+                "scrums_masters": ["Chirvasa Matei", "Ezaru Tudor"]
+            },
+            "database": {
+                "number_of_members": 5,
+                "scrums_masters": ["Mitreanu Alexandru"]
+            },
+            "testing": {
+                "number_of_members": 7,
+                "scrums_masters": ["Ana Maria Ursache"]
+            }
+        }
+    }
+
+    print(test_dict)

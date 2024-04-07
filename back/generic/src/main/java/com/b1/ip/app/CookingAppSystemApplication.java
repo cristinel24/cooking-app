a48d9a019dde38ac3b0a6da5e59d7c56ecb0a164
Calin BALAN <calin.balan03@gmail.com>
2024-04-07 11:14:14 +0300

    [Generic-9] spring boot server first iteration

---
+++ back/generic/src/main/java/com/b1/ip/app/CookingAppSystemApplication.java
@@ -0,0 +1,20 @@
+package com.b1.ip.app;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CookingAppSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CookingAppSystemApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner init() {
+        return args -> {
+            Testing testing = new Testing();
+        };
+    }
+}

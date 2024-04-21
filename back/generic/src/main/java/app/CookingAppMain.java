1a08710c48012a2c38557be30b4cd520baa13fa1
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-04-21 12:44:13 +0300

    remove useless code from main

--- back/generic/src/main/java/app/CookingAppMain.java
+++ back/generic/src/main/java/app/CookingAppMain.java
@@ -1,34 +1,15 @@
 package app;
 
-import app.user.model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationRunner;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 
 @SpringBootApplication
+@Log4j2
 public class CookingAppMain {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(CookingAppMain.class);
-
     public static void main(String[] args) {
         SpringApplication.run(CookingAppMain.class, args);
-        LOGGER.info("Spring Boot application started.");
-    }
-
-    @Bean
-    ApplicationRunner init(MongoTemplate mongoTemplate) {
-        return args -> {
-            try {
-                User user = mongoTemplate.findById("66226029d443d189e5e5ca66", User.class);
-                LOGGER.info("Successfully connected to MongoDB. Retrieved document: {}", user);
-            } catch (Exception ex) {
-                LOGGER.error("Failed to connect to MongoDB: {}", ex.getMessage());
-            }
-        };
+        log.info("Spring Boot application started.");
     }
 }

7b1a2c3234d5b4d0ac2dce2bda21c55130601d0c
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-04-21 15:55:40 +0300

    remove useless grafuri

--- back/ai/utils/process_chatbot.py
+++ back/ai/utils/process_chatbot.py
@@ -66,22 +66,3 @@ async def process_chatbot_query(message_history: list[str], user_query: str,
     )
 
     return chat_completion.choices[0].message.content
-
-
-
-# async def main():
-#     res = await process_chatbot_query(
-#         ["a b", "c d"],
-#         "grafuri?",
-#         ["sos", "dulce", "sarat"],
-#         ["carne", "gluten", "cartofi", "lactate"],
-#         []
-#     )
-#     print(f"{res}")
-# # FOR DEBUG:
-# # m-am luat dupa
-# # https://stackoverflow.com/questions/65682221/runtimeerror-exception-ignored-in-function-proactorbasepipetransport
-# if __name__ == "__main__":
-#     loop = asyncio.new_event_loop()
-#     asyncio.set_event_loop(loop)
-#     loop.run_until_complete(main())

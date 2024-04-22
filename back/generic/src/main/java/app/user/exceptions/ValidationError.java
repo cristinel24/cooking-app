b4424b849375d7add5e1cfe7ffe9ee7ebcdf8b75
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-04-22 08:18:28 +0300

    add a single error handling class

--- back/generic/src/main/java/app/user/exceptions/ValidationError.java
+++
@@ -1,7 +0,0 @@
-package app.user.exceptions;
-
-public class ValidationError extends RuntimeException {
-    public ValidationError(Throwable cause) {
-        super("Validation error", cause);
-    }
-}

6f7e18cb04076e2eb197b9a37d8d46e51ebd177f
AalexandraNicole <126488578+AalexandraNicole@users.noreply.github.com>
2024-04-24 11:01:22 +0300

    patch UserProfile, rename tags, add props

--- front/src/components/UserProfile/index.jsx
+++ front/src/components/UserProfile/index.jsx
@@ -9,69 +9,57 @@ import PageButton from "../PageButton";
 import Page from "../../pages/Page";
 
 function UserProfile(props) {
-  const first_name = props.first_name;
-  const last_name = props.last_name;
-  const followers = props.followers;
-  const following = props.following;
-  const profile_img = props.img;
   const pathPage = "../../pages/Page";
 
   return (
     <div className="user-profile">
       <div className="user-profile-down">
-        <img src={profile_img} alt="" />
-        <div className="user-profile-name">{last_name + " " + first_name}</div>
+        <img src={props.img} alt="" />
+        <div className="user-profile-name">
+          {props.last_name + " " + props.first_name}
+        </div>
         <div className="user-profile-description">
-          <a href="#">{followers} urmaritori</a>{" "}
+          <a href="#">{props.followers} urmaritori</a>{" "}
           <div className="user-profile-line">
             <GoDotFill />
           </div>
-          <a href="#">{following} urmareste</a>
+          <a href="#">{props.following} urmareste</a>
         </div>
-        <Router>
-          <div>
-            <Routes>
-              <Route path={pathPage} element={<Page />} />
-              <Route path={pathPage} element={<Page />} />
-              <Route path={pathPage} element={<Page />} />
-            </Routes>
-            <div className="user-profile-button">
-              <PageButton
-                title={
-                  <>
-                    <FaUserAlt /> Descriere{" "}
-                    <span className="user-profile-button-arrow">
-                      <FaAngleRight />
-                    </span>
-                  </>
-                }
-                path={pathPage}
-              />
-              <PageButton
-                title={
-                  <>
-                    <FaHeart /> Favorite{" "}
-                    <span className="user-profile-button-arrow">
-                      <FaAngleRight />
-                    </span>
-                  </>
-                }
-                path={pathPage}
-              />
-              <PageButton
-                title={
-                  <>
-                    <BsFillGridFill /> Postari{" "}
-                    <span className="user-profile-button-arrow">
-                      <FaAngleRight />
-                    </span>
-                  </>
-                }
-                path={pathPage}
-              />
-            </div>
-          </div>
-        </Router>
+        <div className="user-profile-button">
+          <PageButton
+            title={
+              <>
+                <FaUserAlt /> Descriere{" "}
+                <span className="user-profile-button-arrow">
+                  <FaAngleRight />
+                </span>
+              </>
+            }
+            path={pathPage}
+          />
+          <PageButton
+            title={
+              <>
+                <FaHeart /> Favorite{" "}
+                <span className="user-profile-button-arrow">
+                  <FaAngleRight />
+                </span>
+              </>
+            }
+            path={pathPage}
+          />
+          <PageButton
+            title={
+              <>
+                <BsFillGridFill /> Postari{" "}
+                <span className="user-profile-button-arrow">
+                  <FaAngleRight />
+                </span>
+              </>
+            }
+            path={pathPage}
+          />
+        </div>
         <div className="user-profile-report">
           <a href="#"> Raportează </a>
         </div>

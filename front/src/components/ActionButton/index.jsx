f19140c90ff7b6ba821f2628c5b74795444cc0e6
Loch <ana.26.62.maria.04@gmail.com>
2024-04-25 13:44:52 +0300

    update actionbutton icon

--- front/src/components/ActionButton/index.jsx
+++ front/src/components/ActionButton/index.jsx
@@ -4,7 +4,7 @@ import './index.css'
 function ActionButton({ onClick, text, Icon }) {
     return (
         <button className="action-button" onClick={onClick}>
-            <Icon className="action-button-icon" />
+            {Icon && <Icon className="action-button-icon" />}
             {text && <span className="action-button-text">{text}</span>}
         </button>
     )

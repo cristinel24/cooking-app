a490846a0a81be3d0d800e8c69e471276313d392
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-04-26 14:59:21 +0300

    refactor controller routes

--- back/python/generic/main.py
+++ back/python/generic/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI, APIRouter
+from user.controller import router as user_router
 
 app = FastAPI()
 
-router = APIRouter(
-    prefix="/api/users"
-)
-
+app.include_router(user_router)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
--- back/python/generic/user/controller.py
+++ back/python/generic/user/controller.py
@@ -1,35 +1,38 @@
-import schemas
+from user import schemas
 
 from fastapi import APIRouter
 
-from ..main import router
+router = APIRouter(
+    prefix="/api"
+)
 
 
-@router.get("{name}")
+@router.get("/users/{name}", tags=["users"])
 async def get_user(name: str) -> dict:
     return {"name": name}
 
 
 # session token needed
-@router.patch("{name}/change-data")
+@router.patch("/users/{name}", tags=["users"])
 async def change_account_data(name: str, data: schemas.AccountChangeData) -> dict:
     return {"name": name, "data": data.dict()}
 
 
 # admin action
-@router.delete("{name}/delete-user")
+@router.delete("/users/{name}", tags=["admin"])
 async def delete_user(name: str) -> dict:
     return {"name": name}
 
 
 # admin action
 # todo determine how the role is passed to us
-@router.patch("{name}/change-role")
+@router.patch("/admin/{solver_name}/users/{name}", tags=["admin"])
 async def change_user_role(solver_name: str, name: str, role: int) -> dict:
     return {"name": name, "role": role}
 
 
 # session token needed for the rest of the functions
-@router.patch("{name}/{recipe_name}")
+@router.patch("/users/{name}/saved-recipes/{recipe_name}", tags=["users"])
 async def save_recipe(name: str, recipe_name: str) -> dict:
     return {"name": name, "recipe_name": recipe_name}
+

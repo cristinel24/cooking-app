9f4ea94ab4566f9d108f13b90e9e332cdf8b0d2c
Spiridon Vlad-Iustin <iustin.spiridon@gmail.com>
2024-04-27 16:14:01 +0300

    Update rating_collection.py

--- back/python/db/rating_collection.py
+++ back/python/db/rating_collection.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pymongo.errors
 from pymongo import MongoClient
 
@@ -31,9 +33,21 @@ class RatingCollection(MongoCollection):
         try:
             items = self._collection.find({"recipeId": ObjectId(recipe_id)}).limit(limit)
             ratings = [item for item in items]
+            if not ratings:
+                raise Exception("No ratings found for the provided recipe_id!")
+            return ratings
         except pymongo.errors.PyMongoError as e:
             raise Exception(f"Failed to get ratings by recipe id! - {str(e)}")
-        return ratings
+
+    def get_comments_by_rating_id(self, rating_id: str, limit: int) -> list[str]:
+        try:
+            items = self._collection.find({"parentId": ObjectId(rating_id)}).limit(limit)
+            comment_ids = [str(item["_id"]) for item in items]
+            if not comment_ids:
+                raise Exception("No comments found for the provided rating_id!")
+            return comment_ids
+        except pymongo.errors.PyMongoError as e:
+            raise Exception(f"Failed to get comments by rating id! - {str(e)}")
 
     def get_rating_by_id(self, rating_id: str):
         try:
@@ -50,8 +64,8 @@ class RatingCollection(MongoCollection):
         except pymongo.errors.Any as e:
             raise Exception(f"Failed to insert rating! - {str(e)}")
 
-    def insert_rating(self, name: str, updatedAt: datetime, authorId: ObjectId, recipeId: ObjectId, rating: int,
-                      description: str, parentId: ObjectId, children: list[ObjectId]) -> ObjectId:
+    def insert_rating_with_all_paramether(self, name: str, updatedAt: datetime, authorId: ObjectId, recipeId: ObjectId, rating: int,
+                                          description: str) -> ObjectId:
         try:
             rating_data = {
                 "name": name,
@@ -60,8 +74,6 @@ class RatingCollection(MongoCollection):
                 "recipeId": recipeId,
                 "rating": rating,
                 "description": description,
-                "parentId": parentId,
-                "children": children
             }
             item = self._collection.insert_one(rating_data)
             return item.inserted_id
@@ -70,14 +82,28 @@ class RatingCollection(MongoCollection):
 
     def update_rating(self, rating_id: str, rating_data):
         try:
-            item = self._collection.update_one({"_id": ObjectId(rating_id)}, {"$set": rating_data})
-            return item.modified_count
-        except pymongo.errors.Any as e:
+            result = self._collection.update_one({"_id": ObjectId(rating_id)}, {"$set": rating_data})
+            if result.modified_count > 0:
+                return rating_id
+            else:
+                raise Exception(f"No rating was modified with the ID: {rating_id}")
+        except pymongo.errors.PyMongoError as e:
             raise Exception(f"Failed to update rating! - {str(e)}")
 
     def delete_rating(self, rating_id: str):
         try:
-            item = self._collection.delete_one({"_id": ObjectId(rating_id)})
-            return item.deleted_count
-        except pymongo.errors.Any as e:
+            rating_data = self._collection.find_one({"_id": ObjectId(rating_id)})
+
+            if rating_data:
+                rating_name = rating_data.get("name")
+
+                result = self._collection.delete_one({"_id": ObjectId(rating_id)})
+
+                if result.deleted_count > 0:
+                    return rating_name
+                else:
+                    raise Exception(f"No rating was deleted with the ID: {rating_id}")
+            else:
+                raise Exception(f"No rating found with the ID: {rating_id}")
+        except pymongo.errors.PyMongoError as e:
             raise Exception(f"Failed to delete rating! - {str(e)}")

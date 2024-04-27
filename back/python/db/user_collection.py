ed359fdf05b5c56f76344924c53cd846d63084f1
Spiridon Vlad-Iustin <iustin.spiridon@gmail.com>
2024-04-27 16:22:23 +0300

    Update user_collection.py

--- back/python/db/user_collection.py
+++ back/python/db/user_collection.py
@@ -1,4 +1,3 @@
-import pymongo.errors
 from pymongo import MongoClient
 
 from db.mongo_collection import MongoCollection
@@ -11,32 +10,60 @@ class UserCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.user
 
-    def get_user_by_name(self, user_name: str):
+    def get_user_by_mail(self, mail: str) -> dict:
+        try:
+            item = self._collection.find_one({"email": mail})
+            return item
+        except Exception as e:
+            raise Exception(f"Failed to get user by mail! - {str(e)}")
+
+    def get_user_by_username(self, username: str) -> dict:
+        try:
+            item = self._collection.find_one({"username": username})
+            return item
+        except Exception as e:
+            raise Exception(f"Failed to get user by username! - {str(e)}")
+
+    def get_user_by_name(self, user_name: str) -> dict:
         try:
             item = self._collection.find_one({"name": user_name})
-        # TODO: exception handling
-        except pymongo.errors.Any as e:
+        except Exception as e:
             raise Exception(f"Failed to get user by name! - {str(e)}")
         return item
 
-    def get_user_by_id(self, user_id: str):
+    def get_user_by_id(self, user_id: str) -> dict:
         try:
             item = self._collection.find_one({"_id": ObjectId(user_id)})
-        # TODO: exception handling
-        except pymongo.errors.Any as e:
+        except Exception as e:
             raise Exception(f"Failed to get user by id! - {str(e)}")
         return item
 
-    def insert_user(self, user_data):
+    def insert_user(self, user_data) -> str:
+        """
+        insert a user into the db and returns the id of the newly inserted user
+        :param user_data
+        :return: id of the newly inserted user, as str (must be manually cast to ObjectId)
+        """
         try:
             item = self._collection.insert_one(user_data)
-            return item.inserted_id
-        # TODO: exception handling
-        except pymongo.errors.Any as e:
+            return str(item.inserted_id)
+        except Exception as e:
             raise Exception(f"Failed to insert user! - {str(e)}")
-        return item
+
+    def update_user(self, user_data) -> str:
+        """
+        update a user and return the id of the user
+        :param user_data
+        :return: id of the newly updated user, as str (must be manually cast to ObjectId)
+        """
+        try:
+            item = self._collection.update_one({"_id": user_data["_id"]}, {"$set": user_data})
+            return str(item.upserted_id)
+        except Exception as e:
+            raise Exception(f"Failed to update user! - {str(e)}")
 
 
-if __name__ == "__main__":
-   coll = UserCollection()
-   print(coll.get_user_by_id("662b72721e41bc3685bb71d1"))
+# if __name__ == "__main__":
+#     coll = UserCollection()
+#     print(coll.get_user_by_mail("ionut.dawfawf@gmail.com"))
+#     print(coll.get_user_by_username("ionut.calin"))

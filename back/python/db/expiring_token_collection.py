932f1a2ffcd31dcbb94cba73b5dbdd220b09cff8
Sabina <citrtoj@gmail.com>
2024-04-27 14:15:51 +0300

    add remove_token + some docstrings, make get_user functions return users and not ids

--- back/python/db/expiring_token_collection.py
+++ back/python/db/expiring_token_collection.py
@@ -1,6 +1,4 @@
 from datetime import datetime
-
-import pymongo.errors
 from bson import ObjectId
 
 from db.mongo_collection import MongoCollection
@@ -11,18 +9,32 @@ class ExpiringTokenCollection(MongoCollection):
         super().__init__()
         self._collection = self._connection.cooking_app.expiring_token
 
-    def get_expiring_token(self, token: str):
+    def get_expiring_token_by_value(self, value: str) -> dict:
         try:
-            item = self._collection.find_one({"value": token})
-        # TODO: exception handling
+            item = self._collection.find_one({"value": value})
         except Exception as e:
-            raise Exception(f"Failed to retrieve user context! - {str(e)}")
+            raise Exception(f"Failed to get token! - {str(e)}")
         return item
 
-    def insert_token(self, token: str, user_id: ObjectId, type_token: str):
+    def remove_token(self, token_id: ObjectId):
+        try:
+            result = self._collection.delete_one({"_id": token_id})
+            if result.deleted_count == 0:
+                raise Exception(f"No tokens were removed")
+        except Exception as e:
+            raise Exception(f"Failed to remove token! - {str(e)}")
+
+    def insert_token(self, value: str, user_id: ObjectId, type_token: str) -> int:
+        """
+        insert a new token in the db, return the inserted token's id
+        :param value: token value
+        :param user_id: token's user id
+        :param type_token: token's type
+        :return: ObjectId of the newly inserted token
+        """
         try:
             item = self._collection.insert_one({
-                "value": token,
+                "value": value,
                 "createdAt": datetime.utcnow(),
                 "userId": user_id,
                 "type": type_token
@@ -30,7 +42,6 @@ class ExpiringTokenCollection(MongoCollection):
             return item.inserted_id
         except Exception as e:
             raise Exception(f"Failed to insert token! - {str(e)}")
-        pass
 
 #
 # if __name__ == "__main__":

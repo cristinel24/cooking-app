cabac06461d7b39d2a06e51ce8561c4759d60156
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-04-29 11:34:48 +0300

    implement some auth services

--- front/src/services/auth.jsx
+++ front/src/services/auth.jsx
@@ -1,5 +1,21 @@
-import { axios } from axios;
+import axios from "axios"
 
-const register = (data) => {
-    console.log(process.env.REACT_APP_API_URL)
+const API_URL = import.meta.env.VITE_API_URL
+
+export const register = async (data) => {
+    await axios.post(`${API_URL}/auth/register`, data)
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
+}
+
+export const verify = async (token) => {
+    await axios.post(`${API_URL}/auth/verify?token=${token}`)
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
+}
+
+export const login = async (data) => {
+    await axios.post(`${API_URL}/auth/login`, data)
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
 }

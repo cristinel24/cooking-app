d530cc2b4b3f09cb4317e860acdc8eb1c15851c3
Grozea Tatiana <taneagrozea2003@gmail.com>
2024-05-02 17:33:12 +0300

    change css

--- front/src/components/ReportCard/index.jsx
+++ front/src/components/ReportCard/index.jsx
@@ -15,49 +15,56 @@ function ReportCard(props) {
                 <div className="report-card-header-left">
                     <BsExclamationCircle className="report-card-header-left-img" />
                     <h3 className="report-card-header-left-title">
-                        {props.userName}
+                        {props.title}
                     </h3>
                 </div>
                 <div className="report-card-header-right">
-                    <p>{props.userNumber}</p>
+                    <p>{props.number}</p>
                 </div>
             </div>
             <div className="report-card-content">
                 <p className="report-card-content-description">
-                    {props.content}
+                    Motive:
+                    <br />
+                    {props.reason}
                 </p>
+                <p className="report-card-content-date">Data: {props.date}</p>
             </div>
             <div className="report-card-footer">
-                <div className="report-card-footer-left">
-                    <p className="report-card-footer-left-description">
-                        {props.date}
-                    </p>
+                <div className="report-card-footer-custom-buttons-view">
+                    <PageButton
+                        children={
+                            <>
+                                <span>
+                                    <PiArrowSquareOutBold className="report-card-footer-custom-buttons-img" />
+                                </span>
+                                <span>{props.view}</span>
+                            </>
+                        }
+                        path={props.pathPageView}
+                    />
                 </div>
-                <div className="report-card-footer-right">
-                    <div className="report-card-footer-right-custom-buttons-view">
-                        <PageButton
-                            children={
-                                <>
-                                    <span>
-                                        <PiArrowSquareOutBold className="report-card-footer-right-custom-buttons-img" />
-                                    </span>
-                                    <span>{props.view}</span>
-                                </>
-                            }
-                            path={props.pathPage}
-                        />
-                    </div>
-                    <div className="report-card-footer-right-custom-buttons-read">
-                        <ActionButton
-                            onClick={props.handleClick}
-                            text={
-                                <>
-                                    <span>{read}</span>{' '}
-                                </>
-                            }
-                            Icon={FaCheck}
-                        />
-                    </div>
+                <div className="report-card-footer-custom-buttons-read">
+                    <ActionButton
+                        onClick={props.handleClickRead}
+                        text={
+                            <>
+                                <span>{read}</span>{' '}
+                            </>
+                        }
+                        Icon={FaCheck}
+                    />
+                </div>
+                <div className="report-card-footer-custom-buttons-specific-action">
+                    <ActionButton
+                        onClick={props.handleClickSpecificAction}
+                        text={
+                            <>
+                                <span>{props.specificAction}</span>{' '}
+                            </>
+                        }
+                        Icon={BsExclamationCircle}
+                    />
                 </div>
             </div>
         </div>

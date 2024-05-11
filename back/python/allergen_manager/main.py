f730fd16613a2eea5ae626b2dfb6f4994ff2eaf4
Insomnia2304 <119057832+Insomnia2304@users.noreply.github.com>
2024-05-11 18:48:34 +0300

    add appropiate error handling

--- back/python/allergen_manager/main.py
+++ back/python/allergen_manager/main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, status, Response
 import uvicorn
 import services
+import exceptions
+import constants
 
 load_dotenv()
 
@@ -10,22 +12,33 @@ app = FastAPI()
 
 
 @app.get("/allergen")
-async def get_allergens(starting_with: str):
-    return services.get_allergens_by_starting_string(starting_with)
+async def get_allergens(starting_with: str, response: Response):
+    try:
+        return services.get_allergens_by_starting_string(starting_with)
+    except (Exception,) as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"errorCode": constants.ErrorCodes.SERVER_ERROR.value}
 
 
 @app.post("/allergen/{name}")
-async def add_allergen(name: str):
-    services.add_allergen_by_name(name)
+async def add_allergen(name: str, response: Response):
+    try:
+        services.add_allergen_by_name(name)
+    except (Exception,) as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"errorCode": constants.ErrorCodes.SERVER_ERROR.value}
 
 
 @app.delete("/allergen/{name}")
 async def remove_allergen(name: str, response: Response):
     try:
         services.remove_allergen_by_name(name)
-    except Exception as e:
+    except exceptions.AllergenException as e:
         response.status_code = status.HTTP_406_NOT_ACCEPTABLE
-        return {"errorCode": int(str(e))}
+        return {"errorCode": e.error_code}
+    except (Exception,) as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"errorCode": constants.ErrorCodes.SERVER_ERROR.value}
 
 
 if __name__ == "__main__":
--- back/python/allergen_manager/repository.py
+++ back/python/allergen_manager/repository.py
@@ -1,6 +1,9 @@
 import os
+
+import pymongo
 from pymongo import MongoClient
 import constants
+import exceptions
 
 
 class MongoCollection:
@@ -15,26 +18,31 @@ class AllergenCollection(MongoCollection):
         self._collection = self._connection.cooking_app.allergen
 
     def get_first_allergens_starting_with(self, starting_with: str) -> list[str]:
-        result = self._collection.find({"allergen": {"$regex": f"^{starting_with}"}},
-                                       limit=constants.NO_OF_RETURNED_ITEMS).sort('counter', -1)
+        with pymongo.timeout(constants.MAX_TIMEOUT_TIME_SECONDS):
+            result = self._collection.find(
+                {"allergen": {"$regex": f"^{starting_with}"}},
+                limit=constants.NO_OF_RETURNED_ITEMS
+            ).sort('counter', -1)
 
-        return [item["allergen"] for item in result]
+            return [item["allergen"] for item in result]
 
     def add_allergen_by_name(self, name: str) -> None:
-        result = self._collection.find_one({"allergen": name})
+        with pymongo.timeout(constants.MAX_TIMEOUT_TIME_SECONDS):
+            result = self._collection.find_one({"allergen": name})
 
-        if result:
-            self._collection.update_one(result, {"$inc": {"counter": 1}})
-        else:
-            self._collection.insert_one({"allergen": name, "counter": 1})
+            if result:
+                self._collection.update_one(result, {"$inc": {"counter": 1}})
+            else:
+                self._collection.insert_one({"allergen": name, "counter": 1})
 
     def remove_allergen_by_name(self, name: str) -> None:
-        result = self._collection.find_one({"allergen": name})
+        with pymongo.timeout(constants.MAX_TIMEOUT_TIME_SECONDS):
+            result = self._collection.find_one({"allergen": name})
 
-        if not result:
-            raise Exception(constants.ErrorCodes.NONEXISTENT_ALLERGEN.value)
+            if not result:
+                raise exceptions.AllergenException(constants.ErrorCodes.NONEXISTENT_ALLERGEN.value)
 
-        if result["counter"] == 1:
-            self._collection.delete_one(result)
-        else:
-            self._collection.update_one(result, {"$inc": {"counter": -1}})
+            if result["counter"] == 1:
+                self._collection.delete_one(result)
+            else:
+                self._collection.update_one(result, {"$inc": {"counter": -1}})

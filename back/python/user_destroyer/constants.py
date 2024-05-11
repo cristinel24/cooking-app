40fe5feefcbc1e8926f4b698b0e93772ba2d185e
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-05-11 17:32:37 +0300

    add collections functionality, add service function for route

--- back/python/user_destroyer/constants.py
+++ back/python/user_destroyer/constants.py
@@ -7,3 +7,5 @@ load_dotenv()
 PORT = os.getenv("PORT", 7998)
 HOST_URL = os.getenv("HOST_URL", "0.0.0.0")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017/?directConnection=true")
+
+DELETED_USER_ID = "[deleted]"
--- back/python/user_destroyer/repository.py
+++ back/python/user_destroyer/repository.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
 from constants import MONGO_URL
 
@@ -13,11 +13,24 @@ class FollowCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.follow
 
+    def delete_follows_by_user_id(self, user_id: str):
+        try:
+            self._collection.delete_many({"userId": user_id})
+            self._collection.delete_many({"followsId": user_id})
+        except errors.PyMongoError as e:
+            raise Exception(f"Failed to delete follow relationships! - {str(e)}")
+
 
 class UserCollection(MongoCollection):
     def __init__(self, connection: MongoClient | None = None):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.user
+
+    def delete_user_by_user_id(self, user_id: str):
+        try:
+            self._collection.delete_one({"id": user_id})
+        except errors.PyMongoError as e:
+            raise Exception(f"Failed to delete user! - {str(e)}")
 
 
 class RecipeCollection(MongoCollection):
@@ -25,11 +38,29 @@ class RecipeCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.recipe
 
+    def update_author_id_by_user_id(self, user_id: str, new_user_id: str):
+        try:
+            self._collection.update_many(
+                {"authorId": user_id},
+                {"$set": {"authorId": new_user_id}}
+            )
+        except errors.PyMongoError as e:
+            raise Exception(f"Failed to update recipes for deleted user! - {str(e)}")
+
 
 class RatingCollection(MongoCollection):
     def __init__(self, connection: MongoClient | None = None):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.rating
+
+    def update_author_id_by_user_id(self, user_id: str, new_user_id: str):
+        try:
+            self._collection.update_many(
+                {"authorId": user_id},
+                {"$set": {"authorId": new_user_id}}
+            )
+        except errors.PyMongoError as e:
+            raise Exception(f"Failed to update ratings for deleted user! - {str(e)}")
 
 
 class ReportCollection(MongoCollection):
@@ -37,8 +68,31 @@ class ReportCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.report
 
+    def update_author_id_by_user_id(self, user_id: str, new_user_id: str):
+        try:
+            self._collection.update_many(
+                {"authorId": user_id},
+                {"$set": {"authorId": new_user_id}}
+            )
+        except errors.PyMongoError as e:
+            raise Exception(f"Failed to update deleted user authorId! - {str(e)}")
+
+    def delete_reported_id_by_user_id(self, user_id: str):
+        try:
+            self._collection.delete_many(
+                {"reportedId": user_id}
+            )
+        except errors.PyMongoError as e:
+            raise Exception(f"Failed to delete reportedId! - {str(e)}")
+
 
 class ExpiringTokenCollection(MongoCollection):
     def __init__(self, connection: MongoClient | None = None):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.expiring_token
+
+    def delete_expiring_tokens_by_user_id(self, user_id: str):
+        try:
+            self._collection.delete_many({"userId": user_id})
+        except errors.PyMongoError as e:
+            raise Exception(f"Failed to delete expiring tokens! - {str(e)}")

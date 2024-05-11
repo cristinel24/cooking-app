551499819e7f83f03ddb9f7143d97e28efb48f14
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-05-11 18:22:27 +0300

    add error codes

--- back/python/user_destroyer/constants.py
+++ back/python/user_destroyer/constants.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum
 
 from dotenv import load_dotenv
 
@@ -9,3 +10,9 @@ HOST_URL = os.getenv("HOST_URL", "0.0.0.0")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017/?directConnection=true")
 
 DELETED_USER_ID = "[deleted]"
+
+
+class ErrorCodes(Enum):
+    DB_CONNECTION_FAILURE = 217000
+    NONEXISTENT_USER = 217001
+    

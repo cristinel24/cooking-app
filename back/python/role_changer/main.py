00720e486c6f66a015f5afb357b54ca29f1b044d
user <tudor.ezaru@yahoo.com>
2024-05-12 10:58:55 +0300

    add: changes on pr

--- back/python/role_changer/main.py
+++ back/python/role_changer/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Response
 import uvicorn
 from dotenv import load_dotenv
 from schemas import *
@@ -10,13 +10,14 @@ load_dotenv()
 app = FastAPI()
 
 @app.patch("/admin/users/{user_id}/roles")
-async def update_user_roles(user_id: str, user_roles: RoleData):
+async def update_user_roles(user_id: str, user_roles: RoleData,  response: Response):
     try:
         res = update_user_roles_logic(user_id, user_roles)
-        if res == 0:
-            return { "msg" : "Roles added succesfully" }
-        return { "errorCode" : res }
+        if res != 0:
+            response.status_code = status.HTTP_406_NOT_ACCEPTABLE
+            return { "errorCode" : res }
     except (Exception,) as e:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return { "errorCode": ErrorCodes.SERVER_ERROR.value }
 if __name__ == "__main__":
    uvicorn.run(app, host=os.getenv("HOST", "localhost"), port=int(os.getenv("PORT", 8000)))

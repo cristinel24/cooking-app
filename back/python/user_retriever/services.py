f5fdd10896fd55fb8c027f42d92e7d68c4da2131
mario <mario.guriuc@student.uaic.ro>
2024-05-12 14:00:40 +0300

    refactor code

--- back/python/user_retriever/services.py
+++ back/python/user_retriever/services.py
@@ -35,8 +35,8 @@ async def get_user_full_data(user_id: str) -> UserFullData:
     user_data = user_collection.get_user_by_id(user_id, USER_FULL_DATA_PROJECTION)
     avg_rating = calculate_avg_rating(user_data)
     user_data["ratingAvg"] = avg_rating
-    # follows_count = await request_user_following_count(user_id)
-    # followers_count = await request_user_followers_count(user_id)
-    # user_data["followsCount"] = follows_count
-    # user_data["followersCount"] = followers_count
+    follows_count = await request_user_following_count(user_id)
+    followers_count = await request_user_followers_count(user_id)
+    user_data["followsCount"] = follows_count
+    user_data["followersCount"] = followers_count
     return UserFullData(**user_data)

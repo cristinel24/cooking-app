924cb9c59ed3aac927f76f51328da30f30906f37
Spiridon Vlad-Iustin <iustin.spiridon@gmail.com>
2024-05-12 19:38:29 +0300

    Update constants.py

--- back/python/recipe_retriever/constants.py
+++ back/python/recipe_retriever/constants.py
@@ -1,11 +1,15 @@
 import os
 from enum import Enum
+from dotenv import load_dotenv
 
-PORT = os.getenv("PORT", 8000)
+load_dotenv()
+PORT = os.getenv("PORT", int(8000))
 HOST_URL = os.getenv("HOST_URL", "0.0.0.0")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017/?directConnection=true")
-USER_MICROSERVICE_URL = os.getenv("USER_MICROSERVICE_URL", "http://localhost:7998") #port for user retriever microservice
+USER_MICROSERVICE_URL = os.getenv("USER_MICROSERVICE_URL",
+                                  "http://localhost:7998")  # port for user retriever microservice
 MAX_TIMEOUT_TIME_SECONDS = 3
+
 
 class ErrorCodes(Enum):
     SERVER_ERROR = 20900

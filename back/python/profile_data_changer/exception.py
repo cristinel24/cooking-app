421b7ef2c25f3a7f8fdcf8db0db362a84a9b0c51
mario <mario.guriuc@student.uaic.ro>
2024-05-12 14:33:28 +0300

    fix bug in exception.py

--- back/python/profile_data_changer/exception.py
+++ back/python/profile_data_changer/exception.py
@@ -4,4 +4,4 @@ from constants import ErrorCodes
 class ProfileDataChangerException(Exception):
     def __init__(self, status_code: int, error_code: ErrorCodes):
         self.status_code = status_code
-        self.error_code = error_code.value
+        self.error_code = error_code

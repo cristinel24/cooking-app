a71c10f3ca672bbd2f0f9d27b8ab7c5616658036
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-05-18 13:41:38 +0300

    add transactions

--- back/python/user_destroyer/repository.py
+++ back/python/user_destroyer/repository.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient, errors, timeout
+from pymongo.client_session import ClientSession
 
 from constants import ErrorCodes, MAX_TIMEOUT_TIME_SECONDS, DELETED_USER_ID
 from constants import MONGO_URL
@@ -10,14 +11,17 @@ class MongoCollection:
     _connection = None
 
     def __init__(self, connection: MongoClient | None = None):
-        if self._connection is None:
-            self._connection = MongoClient(MONGO_URL)
-            try:
-                self._connection.admin.command('ping')
-            except ConnectionError:
-                raise UserDestroyerException(ErrorCodes.DB_CONNECTION_FAILURE, 500)
-        else:
+        if connection is not None:
             self._connection = connection
+        else:
+            self._connection = MongoClient(MONGO_URL)
+        try:
+            self._connection.admin.command('ping')
+        except ConnectionError:
+            raise UserDestroyerException(ErrorCodes.DB_CONNECTION_FAILURE, 500)
+
+    def get_connection(self) -> MongoClient:
+        return self._connection
 
 
 class FollowCollection(MongoCollection):
@@ -25,11 +29,11 @@ class FollowCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.follow
 
-    def delete_follows_by_user_id(self, user_id: str):
+    def delete_follows_by_user_id(self, user_id: str, session: ClientSession):
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
                 self._collection.delete_many({"$or": [{"userId": user_id},
-                                                      {"followsId": user_id}]})
+                                                      {"followsId": user_id}]}, session=session)
         except errors.PyMongoError as e:
             raise match_collection_error(e)
 
@@ -39,19 +43,21 @@ class UserCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.user
 
-    def delete_user_by_user_id(self, user_id: str):
+    def delete_user_by_user_id(self, user_id: str, session: ClientSession):
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
-                self._collection.delete_one({"id": user_id})
+                self._collection.delete_one({"id": user_id}, session=session)
         except errors.PyMongoError as e:
             raise match_collection_error(e)
 
     def ping_user(self, user_id: str):
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
-                return self._collection.find_one({"id": user_id}, {"_id": 0, "id": 1})
+                if self._collection.find_one({"id": user_id}, {"_id": 0, "id": 1}) is not None:
+                    return True
         except errors.PyMongoError as e:
             raise match_collection_error(e)
+        return False
 
 
 class RecipeCollection(MongoCollection):
@@ -59,12 +65,13 @@ class RecipeCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.recipe
 
-    def update_author_id_by_user_id(self, user_id: str):
+    def update_author_id_by_user_id(self, user_id: str, session: ClientSession):
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
                 self._collection.update_many(
                     {"authorId": user_id},
-                    {"$set": {"authorId": DELETED_USER_ID}}
+                    {"$set": {"authorId": DELETED_USER_ID}},
+                    session=session
                 )
         except errors.PyMongoError as e:
             raise match_collection_error(e)
@@ -75,12 +82,13 @@ class RatingCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.rating
 
-    def update_author_id_by_user_id(self, user_id: str):
+    def update_author_id_by_user_id(self, user_id: str, session: ClientSession):
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
                 self._collection.update_many(
                     {"authorId": user_id},
-                    {"$set": {"authorId": DELETED_USER_ID}}
+                    {"$set": {"authorId": DELETED_USER_ID}},
+                    session=session
                 )
         except errors.PyMongoError as e:
             raise match_collection_error(e)
@@ -91,21 +99,23 @@ class ReportCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.report
 
-    def update_author_id_by_user_id(self, user_id: str):
+    def update_author_id_by_user_id(self, user_id: str, session: ClientSession):
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
                 self._collection.update_many(
                     {"authorId": user_id},
-                    {"$set": {"authorId": DELETED_USER_ID}}
+                    {"$set": {"authorId": DELETED_USER_ID}},
+                    session=session
                 )
         except errors.PyMongoError as e:
             raise match_collection_error(e)
 
-    def delete_reported_id_by_user_id(self, user_id: str):
+    def delete_reported_id_by_user_id(self, user_id: str, session: ClientSession):
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
                 self._collection.delete_many(
-                    {"reportedId": user_id}
+                    {"reportedId": user_id},
+                    session=session
                 )
         except errors.PyMongoError as e:
             raise match_collection_error(e)
@@ -116,9 +126,9 @@ class ExpiringTokenCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.cooking_app.expiring_token
 
-    def delete_expiring_tokens_by_user_id(self, user_id: str):
+    def delete_expiring_tokens_by_user_id(self, user_id: str, session: ClientSession):
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
-                self._collection.delete_many({"userId": user_id})
+                self._collection.delete_many({"userId": user_id}, session=session)
         except errors.PyMongoError as e:
             raise match_collection_error(e)
--- back/python/user_destroyer/services.py
+++ back/python/user_destroyer/services.py
@@ -1,20 +1,23 @@
 from repository import *
 
-follow_collection = FollowCollection()
-user_collection = UserCollection()
-recipe_collection = RecipeCollection()
-rating_collection = RatingCollection()
-report_collection = ReportCollection()
-expiring_token_collection = ExpiringTokenCollection()
+client = MongoCollection()
+follow_collection = FollowCollection(client.get_connection())
+user_collection = UserCollection(client.get_connection())
+recipe_collection = RecipeCollection(client.get_connection())
+rating_collection = RatingCollection(client.get_connection())
+report_collection = ReportCollection(client.get_connection())
+expiring_token_collection = ExpiringTokenCollection(client.get_connection())
 
 
 async def delete_user(user_id: str):
-    if user_collection.ping_user(user_id) is None:
+    if user_collection.ping_user(user_id) is False:
         raise UserDestroyerException(ErrorCodes.NONEXISTENT_USER, 404)
-    user_collection.delete_user_by_user_id(user_id)
-    follow_collection.delete_follows_by_user_id(user_id)
-    expiring_token_collection.delete_expiring_tokens_by_user_id(user_id)
-    report_collection.update_author_id_by_user_id(user_id)
-    report_collection.delete_reported_id_by_user_id(user_id)
-    recipe_collection.update_author_id_by_user_id(user_id)
-    rating_collection.update_author_id_by_user_id(user_id)
+    with client.get_connection().start_session() as session:
+        with session.start_transaction():
+            user_collection.delete_user_by_user_id(user_id, session)
+            follow_collection.delete_follows_by_user_id(user_id, session)
+            expiring_token_collection.delete_expiring_tokens_by_user_id(user_id, session)
+            report_collection.update_author_id_by_user_id(user_id, session)
+            report_collection.delete_reported_id_by_user_id(user_id, session)
+            recipe_collection.update_author_id_by_user_id(user_id, session)
+            rating_collection.update_author_id_by_user_id(user_id, session)

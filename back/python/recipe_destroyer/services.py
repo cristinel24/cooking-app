3391cde7557fd5bd9884ce0ecf1a8e1c34175a4f
user <tudor.ezaru@yahoo.com>
2024-05-19 20:31:16 +0300

    fixed services and requirements

--- back/python/recipe_destroyer/services.py
+++ back/python/recipe_destroyer/services.py
@@ -16,17 +16,17 @@ async def delete_recipe_service(recipe_id: str):
     if res != 200:
         print("Ratings are not deleted correctly" + res.status_code)
 
-    await delete_tags(tags)
+    res = await delete_tags(tags)
     if res != 200:
         print("Tags are not deleted correctly" + res.status_code)
     
-    await delete_allergens(allergens)
+    res = await delete_allergens(allergens)
     if res != 200:
         print("Allergens are not deleted correctly" + res.status_code)
     
-    ##await delete_image(thumbnail)
-    ##if res != 200:
-      ##  print("Thumbnail is not deleted correctly" + res.status_code)
+    await delete_image(thumbnail)
+    if res != 200:
+        print("Thumbnail is not deleted correctly" + res.status_code)
 
     recipe_collection.delete_recipe(recipe_id)
     user_collection.delete_recipe_from_users(recipe_id, user_id)

d6a1dd86e4a576d954735c1171350625871c9557
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-05-19 12:52:11 +0300

    fix microservices

--- back/python/recipe_saver/exceptions.py
+++ back/python/recipe_saver/exceptions.py
@@ -1,4 +1,5 @@
 class RecipeSaverException(Exception):
     def __init__(self, status_code: int, error_code: int):
         super().__init__()
+        self.status_code = status_code
         self.error_code = error_code

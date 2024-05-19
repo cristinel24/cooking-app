bcf47b79213c0f7a3f49b083c53e388b36fa85c6
mario <mario.guriuc@student.uaic.ro>
2024-05-19 19:10:11 +0300

    update auth verification

--- back/python/profile_data_changer/main.py
+++ back/python/profile_data_changer/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Response, Request, status
+from typing import Annotated
+
+from fastapi import FastAPI, Response, status, Header
 from constants import HOST, PORT, ErrorCodes
 from exception import ProfileDataChangerException
 import services
@@ -9,10 +11,11 @@ app = FastAPI()
 
 
 @app.patch("/{user_id}", tags=["patch_user, auth"])
-async def patch_user(user_id: str, data: UserProfileData, request: Request, response: Response) -> None | dict:
+async def patch_user(user_id: str, data: UserProfileData, response: Response,
+                     x_user_id: Annotated[str | None, Header()] = None) -> None | dict:
     try:
-        if user_id != request.state.user_id:
-            raise ProfileDataChangerException(status.HTTP_403_FORBIDDEN, ErrorCodes.UNAUTHORIZED.value)
+        if user_id != x_user_id:
+            raise ProfileDataChangerException(status.HTTP_403_FORBIDDEN, ErrorCodes.NOT_AUTHENTICATED.value)
         return await services.patch_user(user_id, data)
     except ProfileDataChangerException as e:
         response.status_code = e.status_code

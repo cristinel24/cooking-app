99c726f535b3e549d5cc484a987a1d52fcfa3286
mario <mario.guriuc@student.uaic.ro>
2024-05-19 17:09:46 +0300

    remove unnecesarry print

--- back/python/profile_data_changer/api.py
+++ back/python/profile_data_changer/api.py
@@ -8,7 +8,6 @@ from exception import ProfileDataChangerException
 async def request_add_or_remove_allergens(allergens: list[str], method: str) -> None:
     async with httpx.AsyncClient() as client:
         url = f"{ALLERGEN_MANAGER_API_URL}{method}"
-        print(url)
         payload = json.dumps(allergens)
         response = await client.post(url, content=payload)
         if response.status_code != status.HTTP_200_OK:

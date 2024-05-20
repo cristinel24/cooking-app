3746bf61f5c1b19adccff94ca0fd7a71cc8e62e8
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-05-20 15:21:20 +0300

    add button overflow case

--- front/src/components/Button/index.jsx
+++ front/src/components/Button/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import './index.css'
 
-function Button({ className, onClick, text, Icon }) {
+function Button({ className, onClick, text, Icon, iconAfter }) {
     return (
         <button type="button" className={`action-button ${className}`} onClick={onClick}>
-            {Icon && <Icon className="action-button-icon" />}
+            {!iconAfter && Icon && <Icon className="action-button-icon" />}
             {text && <span className="action-button-text">{text}</span>}
+            {iconAfter && Icon && <Icon className="action-button-icon" />}
         </button>
     )
 }

fdca371351fb71894571b532f01a614615f5c4de
mario <mario.guriuc@student.uaic.ro>
2024-05-21 00:41:39 +0300

    add except for base exception class in main

--- back/python/profile_data_changer/main.py
+++ back/python/profile_data_changer/main.py
@@ -1,28 +1,28 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Response, status, Header
+from fastapi import FastAPI, Header, status
+from fastapi.responses import JSONResponse
 from constants import HOST, PORT, ErrorCodes
 from exception import ProfileDataChangerException
 import services
 import uvicorn
 from schemas import UserProfileData
 
-app = FastAPI()
+app = FastAPI(title="Profile Data Changer")
 
 
-@app.patch("/{user_id}", tags=["patch_user, auth"])
-async def patch_user(user_id: str, data: UserProfileData, response: Response,
-                     x_user_id: Annotated[str | None, Header()] = None) -> None | dict:
+@app.patch("/{user_id}", tags=["patch_user, auth"], response_model=None, response_description="Successful operation")
+async def patch_user(user_id: str, data: UserProfileData,
+                     x_user_id: Annotated[str | None, Header()] = None) -> None | JSONResponse:
     try:
         if user_id != x_user_id:
             raise ProfileDataChangerException(status.HTTP_403_FORBIDDEN, ErrorCodes.UNAUTHORIZED.value)
-        return await services.patch_user(user_id, data)
+        await services.patch_user(user_id, data)
     except ProfileDataChangerException as e:
-        response.status_code = e.status_code
-        return {"errorCode": e.error_code}
-    except (Exception,):
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {"errorCode": ErrorCodes.SERVER_ERROR.value}
+        return JSONResponse(status_code=e.status_code, content={"errorCode": e.error_code})
+    except (Exception,) as e:
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content={"errorCode": ErrorCodes.SERVER_ERROR.value})
 
 
 if __name__ == "__main__":

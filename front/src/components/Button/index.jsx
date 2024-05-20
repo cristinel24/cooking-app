2b7f5fba3c64586b4512f9adebaad2e17fdc2d0f
Sabina <citrtoj@gmail.com>
2024-05-20 15:50:24 +0300

    fix className prop for components

--- front/src/components/Button/index.jsx
+++ front/src/components/Button/index.jsx
@@ -2,7 +2,11 @@ import './index.css'
 
 function Button({ className, onClick, text, Icon, iconAfter }) {
     return (
-        <button type="button" className={`action-button ${className}`} onClick={onClick}>
+        <button
+            type="button"
+            className={`action-button ${className ? className : ''}`}
+            onClick={onClick}
+        >
             {!iconAfter && Icon && <Icon className="action-button-icon" />}
             {text && <span className="action-button-text">{text}</span>}
             {iconAfter && Icon && <Icon className="action-button-icon" />}

f9b0898323be950ddf3563fedc8cd83c4e652517
Chirvasa George Matei <103333398+MediocreKarma@users.noreply.github.com>
2024-05-22 09:19:08 +0300

    remove forgotten method

--- back/python/ai/repository.py
+++ back/python/ai/repository.py
@@ -65,20 +65,3 @@ class DBWrapper:
 
     def get_user_context(self, user_id: str) -> dict:
         return self.__get_from_collection(self._user_collection, user_id)
-
-    def append_to_user_message_history(self, user_id: str, new_message: str):
-        try:
-            with pymongo.timeout(DB_TIMEOUT):
-                pass
-        except AIException as e:
-            raise e
-        except pymongo.errors.PyMongoError as e:
-            if e.timeout:
-                raise AIException(
-                    status_code=status.HTTP_504_GATEWAY_TIMEOUT,
-                    error_code=ErrorCodes.DB_CONNECTION_TIMEOUT
-                )
-            raise AIException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                error_code=ErrorCodes.DB_CONNECTION_NON_TIMEOUT
-            )

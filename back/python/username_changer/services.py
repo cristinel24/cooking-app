56ff9f4e0ac6320409deabbdaa79a34dfb029fb6
Alex Mitreanu <42034413+YorKnEz@users.noreply.github.com>
2024-05-22 22:08:00 +0300

    Update back/python/username_changer/services.py

--- back/python/username_changer/services.py
+++ back/python/username_changer/services.py
@@ -5,6 +5,6 @@ from schemas import UsernameChange
 
 async def handle_change_username(username_change: UsernameChange):
     token_validation_request_response = await request_token_validation(username_change.token)
+    await request_token_destroy(token_validation_request_response.userId)
     db_wrapper = DBWrapper()
     db_wrapper.update_username(token_validation_request_response.userId, username_change.username)
-    await request_token_destroy(token_validation_request_response.userId)

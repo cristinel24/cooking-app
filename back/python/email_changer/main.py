9f10b0656490fa95354ecc27ce0c8d625f3112b4
Calin BALAN <calin.balan03@gmail.com>
2024-05-24 17:52:28 +0300

    standardization

--- back/python/email_changer/main.py
+++ back/python/email_changer/main.py
@@ -1,27 +1,24 @@
-import os
-from dotenv import load_dotenv
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 import uvicorn
-from constants import ErrorCodesToHTTPCodesMapping
+from constants import HOST, PORT, ErrorCodesToHTTPCodesMapping
 from schemas import EmailChange
 from services import handle_change_email
 
-load_dotenv()
-
-app = FastAPI()
+app = FastAPI(title="Email Changer")
 
 
-@app.post("/")
-async def change_email(email_change: EmailChange, response: Response):
+@app.post("/", response_model=None, response_description="Successful operation")
+async def change_email(email_change: EmailChange) -> None | JSONResponse:
     try:
         await handle_change_email(email_change)
     except Exception as e:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         error_code = int(str(e))
         if error_code in ErrorCodesToHTTPCodesMapping:
-            response.status_code = ErrorCodesToHTTPCodesMapping[error_code]
-        return {"errorCode": error_code}
+            status_code = ErrorCodesToHTTPCodesMapping[error_code]
+        return JSONResponse(status_code=status_code, content={"errorCode": error_code})
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=os.getenv("HOST", "localhost"), port=int(os.getenv("PORT", 2600)))
+    uvicorn.run(app, host=HOST, port=PORT)

bb8186c92fab403dbe5a09fb4b636a9d4cbb1fa4
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-05-24 22:18:03 +0300

    add html sanitization

--- back/python/recipe_creator/constants.py
+++ back/python/recipe_creator/constants.py
@@ -6,25 +6,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PORT = int(os.getenv("PORT", 7996))
-HOST = os.getenv("HOST_URL", "0.0.0.0")
+HOST = os.getenv("HOST_URL", "localhost")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/?directConnection=true")
 DB_NAME = os.getenv("DB_NAME")
 
 MAX_TIMEOUT_TIME_SECONDS = 3
 
-ID_GENERATOR_API_URL = os.getenv("ID_GENERATOR_API_URL", "http://0.0.0.0:8002")
+ID_GENERATOR_API_URL = os.getenv("ID_GENERATOR_API_URL", "http://localhost:8002")
 ID_GENERATOR_ROUTE = ID_GENERATOR_API_URL + "/"
 
-AI_API_URL = os.getenv("AI_API_URL", "http://0.0.0.0:8003")
-AI_RECIPE_TOKENIZER_ROUTE = AI_API_URL + ""
+AI_API_URL = os.getenv("AI_API_URL", "http://localhost:8003")
+AI_RECIPE_TOKENIZER_ROUTE = AI_API_URL + "/tokenize/recipe"
 
-ALLERGEN_MANAGER_API_URL = os.getenv("ALLERGEN_MANAGER_API_URL", "http://0.0.0.0:8000")
+ALLERGEN_MANAGER_API_URL = os.getenv("ALLERGEN_MANAGER_API_URL", "http://localhost:8000")
 INC_ALLERGENS_ROUTE = ALLERGEN_MANAGER_API_URL + "/allergens/inc"
 DEC_ALLERGENS_ROUTE = ALLERGEN_MANAGER_API_URL + "/allergens/dec"
 
-TAG_MANAGER_API_URL = os.getenv("TAG_MANAGER_API_URL", "http://0.0.0.0:8001")
+TAG_MANAGER_API_URL = os.getenv("TAG_MANAGER_API_URL", "http://localhost:8001")
 INC_TAGS_ROUTE = TAG_MANAGER_API_URL + "/tags/inc"
 DEC_TAGS_ROUTE = TAG_MANAGER_API_URL + "/tags/dec"
+
+ALLOWED_TAGS = {"p", "ul", "li", "ol", "img", "s", "u", "strong", "em", "br"}
+ALLOWED_ATTRIBUTES = {"img": {"src"}}
+URL_SCHEMES = {"https"}
+UNSAFE_RECIPE_DATA_FIELDS = {"description", "steps"}
 
 
 @unique
@@ -40,3 +45,4 @@ class ErrorCodes(Enum):
     EMPTY_LIST_STEPS = 26108
     EMPTY_LIST_INGREDIENTS = 26109
     INVALID_THUMBNAIL_URL_SIZE = 26110
+    MALFORMED_HTML = 26111
--- back/python/recipe_creator/utils.py
+++ back/python/recipe_creator/utils.py
@@ -1,23 +1,23 @@
+import nh3
 from fastapi import status
 from pymongo import errors
 
-from constants import ErrorCodes
+from constants import *
 from exception import RecipeCreatorException
-from schemas import RecipeData
 
 
-def validate_recipe_data(recipe_data: RecipeData):
-    if not 8 <= len(recipe_data.title) <= 128:
+def validate_recipe_data(recipe_data: dict):
+    if not 8 <= len(recipe_data["title"]) <= 128:
         raise RecipeCreatorException(ErrorCodes.INVALID_TITLE_SIZE.value, status.HTTP_400_BAD_REQUEST)
-    if not 80 <= len(recipe_data.description) <= 10_000:
+    if not 80 <= len(recipe_data["description"]) <= 10_000:
         raise RecipeCreatorException(ErrorCodes.INVALID_DESCRIPTION_SIZE.value, status.HTTP_400_BAD_REQUEST)
-    if recipe_data.prepTime % 5 != 0 or recipe_data.prepTime == 0:
+    if recipe_data["prepTime"] % 5 != 0 or recipe_data["prepTime"] == 0:
         raise RecipeCreatorException(ErrorCodes.INVALID_PREPTIME.value, status.HTTP_400_BAD_REQUEST)
-    if not recipe_data.steps:
+    if not recipe_data["steps"]:
         raise RecipeCreatorException(ErrorCodes.EMPTY_LIST_STEPS.value, status.HTTP_400_BAD_REQUEST)
-    if not recipe_data.ingredients:
+    if not recipe_data["ingredients"]:
         raise RecipeCreatorException(ErrorCodes.EMPTY_LIST_INGREDIENTS.value, status.HTTP_400_BAD_REQUEST)
-    if not 0 <= len(recipe_data.thumbnail) <= 2048:
+    if not 0 <= len(recipe_data["thumbnail"]) <= 2048:
         raise RecipeCreatorException(ErrorCodes.INVALID_THUMBNAIL_URL_SIZE.value, status.HTTP_400_BAD_REQUEST)
 
 
@@ -30,3 +30,24 @@ def match_collection_error(e: errors.PyMongoError) -> RecipeCreatorException:
         return RecipeCreatorException(ErrorCodes.DB_CONNECTION_TIMEOUT.value, status.HTTP_504_INTERNAL_SERVER_ERROR)
     else:
         return RecipeCreatorException(ErrorCodes.DB_CONNECTION_NONTIMEOUT.value, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def sanitize_html(fields: dict[str, str | list[str]]) -> dict[str, str | list[str]]:
+    clean_fields = dict()
+    for key, value in fields.items():
+        if isinstance(value, list):
+            clean_htmls = list()
+            for item in value:
+                clean_htmls.append(
+                    nh3.clean(html=item, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, url_schemes=URL_SCHEMES))
+                if not clean_htmls[-1]:
+                    raise RecipeCreatorException(ErrorCodes.MALFORMED_HTML.value, status.HTTP_400_BAD_REQUEST)
+
+            clean_fields[key] = clean_htmls
+        else:
+            clean_fields[key] = nh3.clean(html=value, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES,
+                                          url_schemes=URL_SCHEMES)
+            if not clean_fields[key]:
+                raise RecipeCreatorException(ErrorCodes.MALFORMED_HTML.value, status.HTTP_400_BAD_REQUEST)
+
+    return clean_fields

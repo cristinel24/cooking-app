cd2cf9f6caa1f851952bde2c231d4ec3c97054c8
Cristian George ANDREI <crandrei@bitdefender.com>
2024-05-26 12:38:44 +0300

    add chatbot microservice

--- back/rust/gateway/src/endpoints/email/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod verify;
-pub use verify::verify_account;
-pub mod request;
-pub use request::request_change;
-
-pub const SERVICE: &str = "email";
--- back/rust/gateway/src/endpoints/role_changer/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod changer;
-pub use changer::admin_role_changer_endpoint;
-
-pub const SERVICE: &str = "role_changer";

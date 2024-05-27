f1477bde5dd227c4ffedd22b19482f543444a825
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-05-27 17:20:59 +0300

    fix dropdown

--- front/src/components/Dropdown/index.jsx
+++ front/src/components/Dropdown/index.jsx
@@ -3,14 +3,16 @@ import '../index'
 
 export default function Dropdown({ options, option, setOption }) {
     return (
-        <select 
-            className="dropdown-item" 
-            value={option} 
-            onChange={(event) => setOption(event.target.value)}
+        <select
+            className="dropdown-item"
+            value={option.name}
+            onChange={(event) =>
+                setOption(options.find((option) => option.name === event.target.value))
+            }
         >
             {options.map((option) => (
-                <option key={option} value={option} className="dropdown-option">
-                    {option}
+                <option key={option.name} value={option.name} className="dropdown-option">
+                    {option.name}
                 </option>
             ))}
         </select>

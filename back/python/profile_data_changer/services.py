421df52d4dcb5d5eb8333c7a9ed7060825d615a5
mario <mario.guriuc@student.uaic.ro>
2024-05-28 01:18:14 +0300

    optimize allergens validation

--- back/python/profile_data_changer/services.py
+++ back/python/profile_data_changer/services.py
@@ -12,12 +12,14 @@ async def patch_user(user_id: str, data: UserProfileData) -> None:
     sanitized_fields = sanitize_html(data.model_dump(include=UNSAFE_USER_DATA_FIELDS))
     for key, value in sanitized_fields.items():
         setattr(data, key, value)
-    changes = {key: value for key, value in vars(UserData(data, user_id)).items() if value is not None}
+    changes = {key: value for key, value in vars(UserData(data)).items() if value is not None}
     validate_user_profile_data(changes)
     allergens_to_add = changes.pop("allergens_to_add")
     allergens_to_remove = changes.pop("allergens_to_remove")
     with client.connection.start_session() as session:
         with session.start_transaction():
-            user_collection.patch_user(user_id, changes, allergens_to_add, allergens_to_remove, session)
+            user_allergens = user_collection.patch_user_and_get_allergens(user_id, changes, allergens_to_add, allergens_to_remove, session)
+            allergens_to_add = [allergen for allergen in allergens_to_add if allergen not in user_allergens]
+            allergens_to_remove = [allergen for allergen in allergens_to_remove if allergen in user_allergens]
             await request_inc_allergens(allergens_to_add) if allergens_to_add else None
             await request_dec_allergens(allergens_to_remove) if allergens_to_remove else None

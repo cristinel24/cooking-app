45bab54102fa27530e0e81f33af64f69a1e898d7
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-05-28 09:12:51 +0300

    add auth and identity check

--- back/python/message_history_manager/utils.py
+++ back/python/message_history_manager/utils.py
@@ -1,11 +1,18 @@
-from pymongo import errors
-from exceptions import MessageHistoryException
-from constants import ErrorCodes
 from fastapi import status
+from fastapi.responses import JSONResponse
+from pymongo import errors
+
+from constants import ErrorCodes
+from exceptions import MessageHistoryException
 
 
 def match_collection_error(e: errors.PyMongoError) -> MessageHistoryException:
     if e.timeout:
         return MessageHistoryException(ErrorCodes.DB_CONNECTION_TIMEOUT, status_code=status.HTTP_504_GATEWAY_TIMEOUT)
     else:
-        return MessageHistoryException(ErrorCodes.DB_CONNECTION_FAILURE, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return MessageHistoryException(ErrorCodes.DB_CONNECTION_FAILURE,
+                                       status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def get_error_json_response(status_code: int, error_code: ErrorCodes) -> JSONResponse:
+    return JSONResponse(status_code=status_code, content={"errorCode": error_code.value})

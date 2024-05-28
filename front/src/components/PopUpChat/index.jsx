71950bbc356909f075d45c0a636755afa33979c3
Loch <ana.26.62.maria.04@gmail.com>
2024-05-28 20:01:36 +0300

    add logged state

--- front/src/components/PopUpChat/index.jsx
+++ front/src/components/PopUpChat/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../../context'
 import Modal from 'react-modal'
 import './index.css'
 import botLogo from '/bot.png'
@@ -7,6 +8,7 @@ import { getResponse } from '../../services/chatbot'
 function PopUpChat() {
     const [error, setError] = useState('')
     const [message, setMessage] = useState('')
+    const { logout, loggedIn } = useContext(UserContext)
     const [loading, setLoading] = useState(true)
     const [modalIsOpen, setModalIsOpen] = useState(false)
 
@@ -37,30 +39,41 @@ function PopUpChat() {
     }
 
     return (
-        <div className="pop-up-chat-container">
-            <img src={botLogo} alt="Logo" className="pop-up-chat-bot-logo" onClick={toggleModal} />
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={toggleModal}
-                contentLabel="chat-modal"
-                className="pop-up-chat-modal-content"
-                overlayClassName="pop-up-chat-modal-overlay"
-            >
-                <div className="pop-up-chat-title">
-                    <h2>Chat</h2>
-                </div>
-                <div className="pop-up-chat-conversation">
-                    <input
-                        className="pop-up-chat-conversation-input"
-                        type="text"
-                        value={message}
-                        onChange={handleInputChange}
-                        onKeyDown={handleKeyPress}
-                        placeholder="Scrie un mesaj..."
+        <>
+            {loggedIn() ? (
+                <div className="pop-up-chat-container">
+                    <img
+                        src={botLogo}
+                        alt="Logo"
+                        className="pop-up-chat-bot-logo"
+                        onClick={toggleModal}
                     />
+                    <Modal
+                        isOpen={modalIsOpen}
+                        onRequestClose={toggleModal}
+                        contentLabel="chat-modal"
+                        className="pop-up-chat-modal-content"
+                        overlayClassName="pop-up-chat-modal-overlay"
+                    >
+                        <div className="pop-up-chat-title">
+                            <h2>Chat</h2>
+                        </div>
+                        <div className="pop-up-chat-conversation">
+                            <input
+                                className="pop-up-chat-conversation-input"
+                                type="text"
+                                value={message}
+                                onChange={handleInputChange}
+                                onKeyDown={handleKeyPress}
+                                placeholder="Scrie un mesaj..."
+                            />
+                        </div>
+                    </Modal>
                 </div>
-            </Modal>
-        </div>
+            ) : (
+                <></>
+            )}
+        </>
     )
 }
 

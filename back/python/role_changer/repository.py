df8e4efe40c33feb90b72a68efd73435451c89f5
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-05-28 17:59:42 +0300

    add admin, auth and identity check

--- back/python/role_changer/repository.py
+++ back/python/role_changer/repository.py
@@ -1,8 +1,8 @@
-from constants import *
-from exceptions import RoleChangerException
 from fastapi import status
 from pymongo import MongoClient, errors, timeout
-from schemas import *
+
+from constants import *
+from exceptions import RoleChangerException
 
 
 class MongoCollection:
@@ -25,26 +25,29 @@ class UserCollection(MongoCollection):
         super().__init__(connection)
         self._collection = self._connection.get_database(DB_NAME).user
 
-
     def get_user_roles(self, user_id: str) -> int:
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
                 user = self._collection.find_one({"id": user_id})
                 if not user:
-                    raise RoleChangerException(status_code=status.HTTP_404_NOT_FOUND, error_code=ErrorCodes.NONEXISTENT_USER.value)
+                    raise RoleChangerException(status_code=status.HTTP_404_NOT_FOUND,
+                                               error_code=ErrorCodes.NONEXISTENT_USER.value)
 
                 return int(user.get("roles", UserRoles.ACTIVE))
         except errors.PyMongoError as e:
             if e.timeout:
-                raise RoleChangerException(status_code=status.HTTP_504_GATEWAY_TIMEOUT, error_code=ErrorCodes.DB_TIMEOUT.value)
-            raise RoleChangerException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, error_code=ErrorCodes.DB_ERROR.value)
-
+                raise RoleChangerException(status_code=status.HTTP_504_GATEWAY_TIMEOUT,
+                                           error_code=ErrorCodes.DB_TIMEOUT.value)
+            raise RoleChangerException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                       error_code=ErrorCodes.DB_ERROR.value)
 
     def update_roles(self, user_id: str, roles: int):
         try:
             with timeout(MAX_TIMEOUT_TIME_SECONDS):
-                self._collection.update_one({"id": user_id}, {"$set": { "roles": roles}})
+                self._collection.update_one({"id": user_id}, {"$set": {"roles": roles}})
         except errors.PyMongoError as e:
             if e.timeout:
-                raise RoleChangerException(status_code=status.HTTP_504_GATEWAY_TIMEOUT, error_code=ErrorCodes.DB_TIMEOUT.value)
-            raise RoleChangerException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, error_code=ErrorCodes.DB_ERROR.value)
+                raise RoleChangerException(status_code=status.HTTP_504_GATEWAY_TIMEOUT,
+                                           error_code=ErrorCodes.DB_TIMEOUT.value)
+            raise RoleChangerException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                       error_code=ErrorCodes.DB_ERROR.value)

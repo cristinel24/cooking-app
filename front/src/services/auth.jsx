4c44470325289f1c44919dad27680fa1bb250f33
Loch <ana.26.62.maria.04@gmail.com>
2024-05-29 18:53:38 +0300

    upsi

--- front/src/services/auth.jsx
+++ front/src/services/auth.jsx
@@ -10,22 +10,8 @@ export const verifyAccount = async (token) => {
     await axios.post(`${API_URL}/verify?token_value=${token}`)
 }
 
-// export const loginUser = async (data) => {
-//     return (await axios.post(`${API_URL}/login`, data)).data
-// }
-
 export const loginUser = async (data) => {
-    return {
-        sessionToken: '1234',
-        user: {
-            id: '1',
-            username: 'jimmy07_dylan04',
-            displayName: 'jimmy',
-            icon: 'https://tazzcdn.akamaized.net/uploads/cover/Cover_Ikura_Sushi_8.png',
-            roles: 2,
-            ratingAvg: 12312.3,
-        },
-    }
+    return (await axios.post(`${API_URL}/login`, data)).data
 }
 
 export const credentialChange = async (email, type) => {

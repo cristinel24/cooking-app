47c3adc03bc5d6ad2957c697548a8841a0318d99
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-05-30 21:43:52 +0300

    add get rating by author

--- back/rust/gateway/src/endpoints/rating_manager/mod.rs
+++ back/rust/gateway/src/endpoints/rating_manager/mod.rs
@@ -6,6 +6,8 @@ pub mod get_all;
 pub use get_all::get_ratings_endpoint;
 pub mod get;
 pub use get::get_rating_endpoint;
+pub mod get_by_author;
+pub use get_by_author::get_rating_by_author_endpoint;
 pub mod patch;
 pub use patch::patch_rating_endpoint;
 pub mod post;

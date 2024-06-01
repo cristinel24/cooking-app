8d8d6bff2d5d19157ae9d381005af3a46815f3ed
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-06-02 01:16:17 +0300

    update allergens service

--- front/src/services/allergens.jsx
+++ front/src/services/allergens.jsx
@@ -3,5 +3,5 @@ import axios from "axios"
 const API_URL = import.meta.env.VITE_API_URL
 
 export const getAllergens = async (startingWith) => {
-    return (await axios.get(`${API_URL}/allergen?starting_with=${startingWith}`)).data.allergens
+    return (await axios.get(`${API_URL}/allergens?starting_with=${startingWith}`)).data.allergens
 }

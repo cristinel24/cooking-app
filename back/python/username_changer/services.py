95450a693c39a73eaee3428072d82303daea0daf
Calin BALAN <calin.balan03@gmail.com>
2024-06-01 11:48:53 +0300

    add request validation to username changer

--- back/python/username_changer/services.py
+++ back/python/username_changer/services.py
@@ -1,9 +1,11 @@
 from api import request_token_validation, request_token_destroy
 from repository import DBWrapper
 from schemas import UsernameChange
+from utils import validate_request
 
 
 async def handle_change_username(username_change: UsernameChange):
+    validate_request(username_change)
     token_validation_request_response = await request_token_validation(username_change.token)
     await request_token_destroy(token_validation_request_response.userId)
     db_wrapper = DBWrapper()

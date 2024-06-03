dfbbc9295f2782e9a6cd838fe135dded622bbd0c
Alex Mitreanu <42034413+YorKnEz@users.noreply.github.com>
2024-06-03 14:12:14 +0300

    Update back/python/recipe_retriever/services.py

--- back/python/recipe_retriever/services.py
+++ back/python/recipe_retriever/services.py
@@ -11,7 +11,7 @@ async def get_recipes_by_user_id(user_id: str, x_user_id: str, start: int, count
     recipes_ids = user_collection.get_user_recipe_ids(user_id, start, count)
     if not recipes_ids:
         return RecipeCardsData(data=[], count=0)
-    return RecipeCardsData(data=await get_recipe_cards(recipes_ids, x_user_id), count=len(recipes_ids))
+    return RecipeCardsData(data=await get_recipe_cards(recipes_ids, x_user_id), total=len(recipes_ids))
 
 async def get_recipes_from_followers(user_id: str, x_user_id: str, start: int, count: int) -> RecipeCardsData:
     following_ids = follow_collection.get_following(user_id)
